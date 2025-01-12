eb99dc272b6a0918bb97c0a3d3780588e0296db4
David <garxia0710@gmail.com>
2025-01-12 17:25:51 -0500

    introducing geometric transformations 💃

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,19 @@ import "./App.css";
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas camera={{position:[2, 2, 2]}}>
-        <mesh>
-          {/* args of diffrent geometries are passed as an array. ' args={['radius', '# of width segments', '# of height segments']} ' */}
-          {/* <sphereGeometry args={[1,80,80]}/> */}
-
-
-          {/* REF offers a lot of geometries like sphere, box, plane, torus, etc. */}
-
-          {/* The args of the box are passed as an array. ' args={['width', 'height', 'depth']} ' */}
-          <boxGeometry args={[1,1,1]}/>
-
-          {/* Some materials require light to be visible. For example, meshPhongMaterial, MeshPhysicalMaterial, etc. */}
-          {/* <meshBasicMaterial color='gold'/> */}
-          <meshPhongMaterial color='gold'/>
-          <directionalLight position={[2, 5, 0]} />
+      <Canvas >
+          {/* We can apply 3 different transformations to the mesh, translation, rotation, and scale */}
+          {/* Position : The position prop is used to move the mesh along the x, y, and z axes. */}
+          {/* Rotation : The rotation prop is used to rotate the mesh around the x, y, and z axes. */}
+          {/* Scale : The scale prop is used to scale the mesh along the x, y, and z axes. */}
+        <mesh 
+        position={[-2, 2, -1]} 
+        rotation={[0, 0 , Math.PI]}
+        scale={[1, 1, 1]}
+        >
+         <torusKnotGeometry args={[1.7, 0.3, 256, 256]} />
+         <meshToonMaterial color={0x00bfff} />
+         <directionalLight position={[2, 5, 1]} />
         </mesh>
       </Canvas>
     </div>
