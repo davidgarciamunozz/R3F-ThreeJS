ef75fe191da3df8d7dff4b23755f8132367f7e5d
David <garxia0710@gmail.com>
2025-01-12 17:13:19 -0500

    📦 Introducing-Meshes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ import "./App.css";
 function App() {
   return (
     <div id="canvas-container">
-      <Canvas>
+      <Canvas camera={{position:[2, 2, 2]}}>
+        <mesh>
+          {/* args of diffrent geometries are passed as an array. ' args={['radius', '# of width segments', '# of height segments']} ' */}
+          {/* <sphereGeometry args={[1,80,80]}/> */}
+
+
+          {/* REF offers a lot of geometries like sphere, box, plane, torus, etc. */}
+
+          {/* The args of the box are passed as an array. ' args={['width', 'height', 'depth']} ' */}
+          <boxGeometry args={[1,1,1]}/>
+
+          {/* Some materials require light to be visible. For example, meshPhongMaterial, MeshPhysicalMaterial, etc. */}
+          {/* <meshBasicMaterial color='gold'/> */}
+          <meshPhongMaterial color='gold'/>
+          <directionalLight position={[2, 5, 0]} />
+        </mesh>
       </Canvas>
     </div>
   );
