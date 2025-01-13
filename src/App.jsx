683c92e3abaa64e515af90f40405f4503127438e
David <garxia0710@gmail.com>
2025-01-12 19:00:56 -0500

    Camera controls (Firstperson - Orbit) 🎥

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { FirstPersonControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
+
+// for camera controls we need to install the following package '@react-three/drei'
+// wich is a collection of useful helpers and abstractions for react-three-fiber
 
 function AnimatedBox() {
   const boxRef = useRef();
@@ -20,10 +25,19 @@ function AnimatedBox() {
   )
 }
 
+// FirstPersonControls is a component that allows us to move the camera like a first person game
+// movementSpeed is the speed of the camera
+// other props are available like lookSpeed, lookVertical, enabled, heightCoef, heightMin, heightMax, width, autoForward, etc
+
+// OrbitControls is a component that allows us to move the camera like a third person game
+// other props are available like enableZoom, enablePan, enableDamping, dampingFactor, enableRotate, rotateSpeed, zoomSpeed, panSpeed, etc
+
 function App() {
   return (
     <div id="canvas-container">
       <Canvas >
+        {/* <FirstPersonControls movementSpeed={2} /> */}
+        <OrbitControls />
          <AnimatedBox/>
          <directionalLight position={[2, 5, 1]} />
       </Canvas>
